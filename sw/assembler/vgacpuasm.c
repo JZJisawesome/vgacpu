e47830a82e90cec3f41893b80ce99424795fb50e
John Jekel <JZJisawesome123@gmail.com>
2022-07-02 21:49:34 -0400

    Workon assembler more

--- sw/assembler/vgacpuasm.c
+++ sw/assembler/vgacpuasm.c
@@ -8,17 +8,16 @@
 /* Constants And Defines */
 
 #define NUM_INSTRUCTIONS 44
+#define MAIN_MEMORY_SIZE 16384
 
 /* Includes */
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 /* Types */
-
-//TODO
-
-/* Variables */
 
 typedef struct string_opcode_lookup_group {
     const char* instruction;//Lowercase of instruction
@@ -31,6 +30,8 @@ typedef struct string_opcode_lookup_group {
         };
     };
 } string_opcode_lookup_group;
+
+/* Variables */
 
 static const string_opcode_lookup_group string_opcode_lookup [NUM_INSTRUCTIONS] = {
     //Type 3
@@ -90,23 +91,85 @@ static const string_opcode_lookup_group string_opcode_lookup [NUM_INSTRUCTIONS] 
 
 /* Static Function Declarations */
 
-//TODO
+static void assemble_into_memory_image(const char* file_data, uint8_t* memory_image);
+static const string_opcode_lookup_group* parse_instruction_line(const char* line);
+static size_t get_file_size(FILE* file);
 
 /* Function Implementations */
 
 int main(int argc, const char** argv) {
+
+    //We start with a bunch of error checking
     if (argc < 3) {
         fputs("Error: Invalid number of arguments (expected 2)!\n", stderr);
 
         const char* bin_name = argc ? argv[0] : "vgacpuasm";
-        fprintf(stderr, "Usage: %s INPUT.s OUTPUT.bin!\n", bin_name);
+        fprintf(stderr, "Usage: %s INPUT.s OUTPUT.bin\n", bin_name);
         return 1;
     }
 
-    fputs("Error: Not implemented!\n", stderr);
-    return 1;
+    //Load the contents of the input file into a buffer
+    FILE* input_file = fopen(argv[1], "r");
+    if (!input_file) {
+        fprintf(stderr, "Error: Failed to open %s for reading.\n", argv[1]);
+        return 1;
+    }
+
+    size_t input_file_size = get_file_size(input_file);
+    char* input_file_data = (char*) malloc(sizeof(char) * input_file_size);
+    if (!input_file_data) {
+        fprintf(stderr, "Error: The file you supplied is too big. Please try again.\n");
+        fclose(input_file);
+        return 1;
+    }
+    fread(input_file_data, sizeof(char), input_file_size, input_file);
+    fclose(input_file);//We're done with the file now
+
+    //Cheaper to just keep this on the stack than malloc and free things :)
+    //I'd do this for input_file_data too but we don't know its size at
+    //compile time and even if we use alloca the file size could
+    //blow past the amount of stack we have available
+    uint8_t memory_image[MAIN_MEMORY_SIZE];
+
+    //Perform the actual assembly and write to the memory_image buffer
+    assemble_into_memory_image(input_file_data, memory_image);
+    free(input_file_data);//No more need for this
+
+    //Write the memory image out as a binary file and close it
+    FILE* output_file = fopen(argv[2], "wb");
+    if (!output_file) {
+        fprintf(stderr, "Error: Failed to open %s for writing.\n", argv[2]);
+        fclose(input_file);
+        return 1;
+    }
+    fwrite(memory_image, sizeof(uint8_t), MAIN_MEMORY_SIZE, output_file);
+    fclose(output_file);
+
+    //We're done!
+    fputs("Done!\n", stderr);
+    return 0;
 }
 
 /* Static Function Implementations */
 
-//TODO
+static void assemble_into_memory_image(const char* file_data, uint8_t* memory_image) {
+    assert(file_data);
+    assert(memory_image);
+    //TODO implement
+}
+
+static const string_opcode_lookup_group* parse_instruction_line(const char* line) {
+    assert(line);
+    //TODO implement
+}
+
+static size_t get_file_size(FILE* file) {
+    assert(file);
+
+    //TODO implement this in a more foolproof way
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    rewind(file);
+
+    return size;
+}
