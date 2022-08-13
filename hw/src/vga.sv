f7523578d66ead5dc29f1bd81c37122a4d13f2ce
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-08-12 21:32:24 -0400

    Work on VGA module

--- hw/src/vga.sv
+++ hw/src/vga.sv
@@ -2,21 +2,75 @@
  * By: John Jekel
  *
  * Module for outputing VGA video from a framebuffer.
+ * As planned, this outputs 640x480 video, but the framebuffer it reads from
+ * is only 214x160.
+ *
+ * Useful: http://tinyvga.com/vga-timing/640x480@60Hz
  *
 */
 module vga
 (
     input logic clk,//50MHz
-    input logic n_rst,
+    input logic rst_async,
 
     //VGA Outputs (640x480)
     output logic vga_r, vga_g, vga_b,
-    output logic vga_hsync, vga_vsync
+    output logic vga_hsync, vga_vsync,
 
     //Framebuffer access
-    //TODO
+    output logic [15:0] fb_addr,
+    input logic [2:0] fb_pixel
 );
 
+/* VGA Timing Parameters */
+
+//TODO defining 640x480 VGA timings as parameters here
+//localparam X_DRAW_PERIOD = TODO;//In counter ticks (equivalent to clock cycles)
+//localparam X_BLANK_PERIOD = TODO;//In counter ticks (equivalent to clock cycles)
+
+//localparam X_
+
+//X and Y length are INCLUSIVE
+//localparam X_MAX = X_DRAW_PERIOD + X_BLANK_PERIOD - 1;
+//localparam Y_MAX = Y_DRAW_PERIOD + X_BLANK_PERIOD - 1;
+
+//TESTING values just to try things out
+localparam X_MAX = 420;
+localparam Y_MAX = 69;
+
+/* X and Y Counters */
+//These DO NOT count pixels, but rather 50MHz clock pulses
+logic [10:0] x_cnt;
+logic [10:0] y_cnt;
+
+always_ff @(posedge rst_async, posedge clk) begin
+    if (rst_async) begin
+        x_cnt <= '0;
+        y_cnt <= '0;
+    end else if (clk) begin
+        if (x_cnt == X_MAX) begin//We are at the end of a line
+            x_cnt <= '0;//Reset x_cnt
+            y_cnt <= (y_cnt == Y_MAX) ? '0 : (y_cnt + 1);//Reset y_cnt if we are currently at y's max too; else increment it
+        end else//Otherwise just increment x_cnt
+            x_cnt <= (x_cnt + 1);
+    end
+end
+
+//TESTING
+assign fb_addr = x_cnt;
+
 //TODO
+
+//TODO plan out the timing of this (need to fetch pixel data from framebuffer in time for VGA timing/etc)
+//Perhaps have independent x and y counters continuously incrementing + wrapping after the corrent count
+//HSYNC and YSYNC can be directly driven based off of those
+//Those counters can serve as inputs to a state machine that deals with actually drawing pixels/driving the r g and b lines,
+//accessing the FB, etc
+
+//So the pixel clock for our desired VGA signal is 25MHz, and we have double that. This gives us more breathing room!
+
+
+//TESTING ensuring FPGA can support only 3 bit wide sram (unlikely, will likely need to rethink things)
+
 
 endmodule
--- hw/src/vgacpu_top.sv
+++ hw/src/vgacpu_top.sv
@@ -6,7 +6,7 @@
 */
 module vgacpu_top (
     input logic clk,
-    input logic n_rst,
+    input logic n_rst_async,
 
     //TODO button inputs, VGA outputs
 
@@ -15,24 +15,50 @@ module vgacpu_top (
     output logic vga_hsync, vga_vsync
 );
 
-//Clocking
+/* Clocking And Reset */
+
+logic rst_async;
+assign rst_async = ~n_rst_async;
+
 //This system expects a 50MHz clock. In the case that clk is not 50MHz, you can setup a PLL
 //or some sort of simple divider here. For me I'm just passing thru clk.
 logic clk_50;
 assign clk_50 = clk;
 
+/* Connections Between CPU, Rasterizer, VGA Module, etc */
 
-//Connections Between CPU, Rasterizer, VGA Module, etc
 //TODO
 
+//Framebuffer-VGA Output Module connections
+logic [15:0] vga_fb_addr;
+logic [2:0] vga_fb_pixel;
 
-//TESTING sram
+/* Module instantiations */
+
+//Framebuffer
 inferred_sram #(
-    .INITIALIZE_FROM_FILE(0),
-	.D_WIDTH(8),
-	.A_WIDTH(8)
-) test_sram (
-    .clk(clk_50)
+    .INITIALIZE_FROM_FILE(0),//TODO eventually do init this for file (maybe show some default logo/for testing)
+	.D_WIDTH(3),
+	.A_WIDTH(16)//214x160 pixels
+) framebuffer (
+    .clk(clk_50),
+    .read_addr_a(vga_fb_addr),
+    .read_a(vga_fb_pixel)
+    //TODO other connections
+);
+
+//VGA Output Module
+vga vga_output_controller (
+    .clk(clk_50),
+    .rst_async(rst_async),
+
+    //VGA Outputs (640x480)
+    .vga_r(vga_r), .vga_g(vga_g), .vga_b(vga_b),
+    .vga_hsync(vga_hsync), .vga_vsync(vga_vsync),
+
+    //Framebuffer access
+    .fb_addr(vga_fb_addr),
+    .fb_pixel(vga_fb_pixel)
 );
 
 endmodule
