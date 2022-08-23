dc7534dfc6a4e2786aae278d8abfb03f2bb5e248
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-08-23 09:21:33 -0400

    Switch to verilator for simulation

---
+++ hw/tb/verilator/verilator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Based on script from JZJCoreF
+
+FILES_TO_INCLUDE="-I ../../src/common.sv -I ../../src/rasterizer.sv -I ../../src/framebuffer.sv -I ../../src/vgacpu_top.sv -I ../../src/sound.sv -I ../../src/vga.sv -I ../../src/buttons.sv -I ../../src/cpu/cpu_common.sv  -I ../../src/cpu/vgacpu.sv -I ../../src/cpu/control.sv -I ../../src/cpu/main_mem.sv -I ../../src/cpu/alu.sv -I ../../src/cpu/agu.sv -I ../../src/cpu/reg_file.sv -I ../../src/cpu/rf_mux.sv -I ../../src/cpu/sp.sv -I ../../src/cpu/fetch.sv -I ../../src/cpu/decode.sv -I ../../src/cpu/pr.sv -I ../../src/cpu/rasterizer_controller.sv -I ../../src/cpu/snd_controller.sv -I ../../src/cpu/button_controller.sv"
+
+#Verilate the testbench and vgacpu SystemVerilog files//todo split into multiple commands
+verilator $FILES_TO_INCLUDE --timescale 10ns/10ns -Wall -Wno-fatal -sv -cc verilator.sv --exe --trace-fst -O3 --top-module vgacpu_verilator +1800-2017ext+sv --build verilator.cpp
+#Run the simulation (creates /tmp/vgacpu_verilator.vcd)
+./obj_dir/Vvgacpu_verilator
+#Open in waveform viewer
+gtkwave /tmp/vgacpu_verilator.vcd
+#Delete files
+rm -rf ./obj_dir
+rm /tmp/vgacpu_verilator.vcd
---
+++ hw/tb/verilator/verilator.sv
@@ -0,0 +1,32 @@
+//Intended for use with verilator
+//Based on tb from JZJCoreF
+
+module vgacpu_verilator (
+    input logic clk,
+    input logic n_rst_async,
+    input logic [3:0] buttons_async
+);
+
+logic vga_r, vga_g, vga_b;
+logic vga_hsync, vga_vsync;
+
+logic buzzer;
+
+vgacpu_top top (.*);
+
+initial begin
+    $dumpfile("/tmp/vgacpu_verilator.vcd");
+    $dumpvars(0, vgacpu_verilator);
+end
+
+//Clock cycle counter to end simulation
+logic [63:0] counter = 0;
+
+always_ff @(posedge clk) begin
+    counter <= counter + 1;
+
+    if (counter == 1000000)
+        $finish();
+end
+
+endmodule
